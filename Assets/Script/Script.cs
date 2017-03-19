3e8e6b904404bbde424e9928ddc10c9e5ed392f1
Vlad Pnevsk <vlad.007.p@gmail.com>
2017-03-19 19:12:41 +0200

    first commit

---
+++ Assets/Script/Script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody rb;
+
+     void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+     void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+        rb.AddForce(direction * Speed);
+    }
+
+
+}
+
+
